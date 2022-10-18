d56d0a3ab422001d2af15235e3742dc62a558985
Giulyano Santos <giulyanofelipesantos@gmail.com>
2022-10-18 19:00:25 -0300

    First commit :tada:

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(
+  "#app > section > div.cc-bg > svg > g > g:nth-child(1) > path"
+);
+
+const ccBgColor02 = document.querySelector(
+  "#app > section > div.cc-bg > svg > g > g:nth-child(2) > path"
+)
+
+// ccBgColor01.setAttribute('fill', 'blue');  //setando o atributo do elemento path
+// ccBgColor02.setAttribute("fill", 'green');
+
